c6d3d4e7e82504eb040e9351928cd032d915f5a5
brettl1991 <brettl1991@gmail.com>
2022-09-11 13:13:44 +0100

    Add experience section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -13,6 +13,7 @@ const Skills = () => {
     const query = '*[_type == "experiences"]';
     const skillsQuery = '*[_type == "skills"]';
     client.fetch(query).then((data) => {
+      console.log(data);
       setExperience(data);
     });
     client.fetch(skillsQuery).then((data) => {
@@ -24,7 +25,7 @@ const Skills = () => {
       <h2 className="head-text">Skills & Experience</h2>
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills.map((skill) => (
+          {skills?.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
@@ -39,6 +40,31 @@ const Skills = () => {
               </div>
               <p className="p-text">{skill.name}</p>
             </motion.div>
+          ))}
+        </motion.div>
+        <motion.div className="app__skills-exp">
+          {experience?.works?.map((work) => (
+            <>
+              <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="app__skills-exp-work"
+                data-tip
+                data-for={work.name}
+                key={work.name}
+              >
+                <h4 className="bold-text">{work.name}</h4>
+                <p className="p-text">{work.company}</p>
+              </motion.div>
+              <ReactTooltip
+                id={work.name}
+                effect="solid"
+                arrowColor="#fff"
+                className="skills-tooltip"
+              >
+                {work.desc}
+              </ReactTooltip>
+            </>
           ))}
         </motion.div>
       </div>
